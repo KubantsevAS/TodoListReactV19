53890eb87d96f5e59fd7161d5370f593981bcbd1
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-01-02 03:30:47 +0300

    init: setup project

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { UsersPage } from "../pages/users";
+import { TodoListPage } from "../pages/todo-lists";
+
+fetch('http://localhost:3001/users').then(res => console.log(res));
+
+export function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<UsersPage />} />
+            <Route path="/:userId/tasks" element={<TodoListPage />} />
+        </Routes>
+    );
+}
