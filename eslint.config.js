c31e7b2c10e79ea07b31a2ebaf8e6e7e713619a8
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-01-05 02:20:14 +0300

    refactor: update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import stylisticJs from '@stylistic/eslint-plugin-js';
+import stylisticTs from '@stylistic/eslint-plugin-ts';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
@@ -17,7 +18,8 @@ export default tseslint.config(
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
-            '@stylistic/js': stylisticJs
+            '@stylistic/js': stylisticJs,
+            '@stylistic/ts': stylisticTs,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
@@ -26,28 +28,35 @@ export default tseslint.config(
                 { allowConstantExport: true },
             ],
             '@stylistic/js/array-bracket-spacing': ['error', 'never'],
-            '@stylistic/js/comma-dangle': ['error', 'always-multiline'],
             '@stylistic/js/eol-last': 'error',
-            '@stylistic/js/indent': ['error', 4, { SwitchCase: 1 }],
             '@stylistic/js/jsx-quotes': ['error', 'prefer-single'],
             '@stylistic/js/max-len': ['error', { 'code': 120 }],
-            '@stylistic/js/no-extra-semi': 'error',
             '@stylistic/js/no-multi-spaces': 'error',
             '@stylistic/js/no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0, 'maxBOF': 0 }],
             '@stylistic/js/no-trailing-spaces': 'error',
-            '@stylistic/js/object-curly-spacing': ['error', 'always'],
             '@stylistic/js/one-var-declaration-per-line': ['error', 'always'],
             '@stylistic/js/padded-blocks': ['error', 'never'],
-            '@stylistic/js/padding-line-between-statements': [
+            '@stylistic/js/semi-spacing': 'error',
+            '@stylistic/js/semi-style': ['error', 'last'],
+            '@stylistic/js/space-in-parens': 'error',
+            '@stylistic/ts/comma-dangle': ['error', 'always-multiline'],
+            '@stylistic/ts/indent': ['error', 4, { SwitchCase: 1 }],
+            '@stylistic/ts/member-delimiter-style': 'error',
+            '@stylistic/ts/no-extra-semi': 'error',
+            '@stylistic/ts/object-curly-newline': ['error', { 'multiline': true, 'minProperties': 4, 'consistent': true }],
+            '@stylistic/ts/object-curly-spacing': ['error', 'always'],
+            '@stylistic/ts/object-property-newline': ['error', {'allowAllPropertiesOnSameLine': false}],
+            '@stylistic/ts/padding-line-between-statements': [
                 'error',
                 { blankLine: 'always', prev: '*', next: ['export', 'return', 'function'] }
             ],
-            '@stylistic/js/quotes': ['error', 'single'],
-            '@stylistic/js/semi': 'error',
-            '@stylistic/js/semi-spacing': 'error',
-            '@stylistic/js/semi-style': ['error', 'last'],
-            '@stylistic/js/space-before-blocks': 'error',
-            '@stylistic/js/space-in-parens': 'error',
+            '@stylistic/ts/quotes': ['error', 'single'],
+            '@stylistic/ts/semi': 'error',
+            '@stylistic/ts/space-before-blocks': 'error',
+            '@stylistic/ts/type-annotation-spacing': [
+                'error',
+                { 'before': false, 'after': true, 'overrides': { 'arrow': { 'before': true, 'after': true } } },
+            ],
             'no-console': 'error',
         },
     }
