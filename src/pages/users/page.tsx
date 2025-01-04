afb40694194c6c9c7359a10d2e92d5a3eedf2f64
Aleksandr Kubantsev <aleksandr.kubantsev@simpleone.ru>
2025-01-04 03:32:39 +0300

    feat: setup simple useState logic for users page

--- src/pages/users/page.tsx
+++ src/pages/users/page.tsx
@@ -1,25 +1,55 @@
 import { useState } from 'react';
 
 type User = {
-  id: string;
-  email: string;
+    id: string;
+    email: string;
 };
 
 export function UsersPage() {
     const [users, setUsers] = useState<User[]>([]);
+    const [email, setEmail] = useState<string>('');
+
+    const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setUsers([...users, { id: crypto.randomUUID(), email }]);
+        setEmail('');
+    };
+    const deleteUser = (selectedUserId:string) => {
+        setUsers(users.filter(user => user.id !== selectedUserId));
+    };
 
     return (
-        <main>
-            <h1 className="text-3x1 font-bold underline">Users</h1>
+        <main className='flex flex-col gap-4 container mx-auto p-4 pt-10'>
+            <h1 className="text-3xl font-bold underline">Users</h1>
             <section>
-                <form></form>
+                <form className="flex gap-2" onSubmit={handleSubmit}>
+                    <input
+                        className="border p-2 rounded"
+                        type="email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        type="submit"
+                    >
+                        Add
+                    </button>
+                </form>
             </section>
-            <section>
-                <div className="flex flex-col">
-                    {users.map((user) => (
-                        <div key={user.id}>{user.email}</div>
-                    ))}
-                </div>
+            <section className="flex flex-col gap-2">
+                {users.map((user) => (
+                    <div className="flex items-center gap-2 border p-2 rounded bg-gray-100" key={user.id}>
+                        {user.email}
+                        <button
+                            className='bg-red-500 hover:bg-red-700 font-bold py-2 px-4 rounded ml-auto'
+                            type='button'
+                            onClick={() => deleteUser(user.id)}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                ))}
             </section>
         </main>
     );
